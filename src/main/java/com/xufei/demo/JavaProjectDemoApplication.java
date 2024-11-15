7df65aa3b165ffa7af078144eabc08182734694d
xufei <>
2024-11-15 17:34:40 +0800

    feat:初始化项目

---
+++ src/main/java/com/xufei/demo/JavaProjectDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xufei.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProjectDemoApplication.class, args);
+    }
+
+}
