7b92938451f0b3e0faa770fcba83a84ebd74f778
XIAOBAICAIJI-66 <80099295+XIAOBAICAIJI-66@users.noreply.github.com>
2024-11-15 17:41:08 +0800

    Update JavaProjectDemoApplicationTests.java
    测试github拉取

--- src/test/java/com/xufei/demo/JavaProjectDemoApplicationTests.java
+++ src/test/java/com/xufei/demo/JavaProjectDemoApplicationTests.java
@@ -3,6 +3,7 @@ package com.xufei.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//启动类
 @SpringBootTest
 class JavaProjectDemoApplicationTests {
 
